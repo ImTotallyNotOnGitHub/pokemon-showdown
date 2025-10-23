eb456ced342c5117f09ffc2b1e7eb230603f7498
ImTotallyNotOnGitHub <franzandrew16@gmail.com>
2025-10-23 14:02:55 -0400

    Unite Mons Start

---
+++ data/mods/Unitemons/pokedex.ts
@@ -0,0 +1,402 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	venusaur: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	charizard: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	blastoise: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	pikachu: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	raichualola: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	clefable: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	ninetalesalola: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	wigglytuff: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	psyduck: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	machamp: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	rapidashgalar: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	dodrio: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	gengar: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mrmime: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	scyther: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	gyarados: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	lapras: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	snorlax: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	dragonite: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mewtwo: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mewtwomegax: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mewtwomegay: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mew: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	azumarill: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	espeon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	umbreon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	scizor: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	blissey: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	tyranitar: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	hooh: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	blaziken: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	gardevoir: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	sableye: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	absol: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	metagross: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	latias: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	latios: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	empoleon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	garchomp: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	lucario: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	lucariomega: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	leafeon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	glaceon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mamoswine: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	darkrai: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	crustle: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	zoroark: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	chandelure: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	delphox: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	greninja: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	talonflame: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	aegislash: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	aegislashblade: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	sylveon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	goodra: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	trevenant: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	decidueye: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	tsareena: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	comfey: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mimikyu: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	mimikyubusted: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	cinderace: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	inteleon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	greedent: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	eldegoss: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	cramorant: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	cramorantgulping: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	cramorantgorging: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	alcremie: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	falinks: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	duraludon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	dragapult: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	zaciancrowned: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	urshifu: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	urshifurapidstrike: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	pawmot: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	armarouge: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	ceruledge: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	tinkaton: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	},
+	miraidon: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", H: "Chlorophyll"},
+	}
+};
